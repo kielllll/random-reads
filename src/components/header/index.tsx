4b3167d01a7cb0495365efaa02f6eca4d9e558ac
kielllll <eatarranza@gmail.com>
2024-07-07 22:13:14 +0800

    feat: initial setup for header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,19 @@
+import Link from 'next/link'
+import { Switch } from '../ui/switch'
+import { Label } from '../ui/label'
+
 export default function Header() {
   return (
-    <header className="flex w-full max-w-5xl items-center justify-between py-4">
-      <p>logo</p>
-      <nav className="flex justify-between gap-4">
-        <a>Home</a>
-        <a>Blogs</a>
+    <header className="flex w-full max-w-5xl items-center justify-between py-4 relative">
+      <Link href="/">Logo</Link>
+      <nav className="flex justify-between gap-4 absolute right-1/2">
+        <Link href="/">Home</Link>
+        <Link href="/blogs">Blogs</Link>
       </nav>
-      <p>toggle</p>
+      <div className="flex items-center space-x-2">
+        <Switch id="dark-mode" />
+        <Label htmlFor="dark-mode">Dark Mode</Label>
+      </div>
     </header>
   )
 }
