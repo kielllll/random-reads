1865d17b2a46d205ec7e3264006ff5fd2f237e4b
kielllll <eatarranza@gmail.com>
2024-07-15 22:28:31 +0800

    feat: add home logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,19 @@
 import Link from 'next/link'
 import { Switch } from '../ui/switch'
 import { Label } from '../ui/label'
+import Image from 'next/image'
 
 export default function Header() {
   return (
     <header className="flex w-full max-w-4xl items-center justify-between py-4 relative">
-      <Link href="/">Logo</Link>
+      <Link href="/">
+        <Image
+          src={'/random-reads.svg'}
+          width={50}
+          height={50}
+          alt={'Random Reads'}
+        />
+      </Link>
       <div className="flex items-center space-x-2">
         <Switch id="dark-mode" />
         <Label htmlFor="dark-mode">Dark Mode</Label>
