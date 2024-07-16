e6cba48e3521593df15a1fe7e668507d8d1c4b4f
kielllll <eatarranza@gmail.com>
2024-07-16 20:44:16 +0800

    feat: banner; responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Banner from './_components/Banner'
 import Blogs from './_components/Blogs'
 
 export default function Home() {
   return (
-    <main className="w-full max-w-4xl flex flex-col gap-8">
-      <div>Banner</div>
+    <main className="w-full max-w-4xl flex flex-col gap-4 items-center py-6">
+      <Banner />
       <Blogs />
     </main>
   )
