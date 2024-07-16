39efb043841bbfed139b68f038cfce534d9845d0
kielllll <eatarranza@gmail.com>
2024-07-16 20:53:25 +0800

    feat: add image on blog post page and responsive layout

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,5 +1,7 @@
+import { generateImageUrl } from '@/lib/sanity'
 import { getPostBySlug } from '@/server/post'
 import { PortableText } from 'next-sanity'
+import Image from 'next/image'
 import Link from 'next/link'
 import { FaLongArrowAltLeft } from 'react-icons/fa'
 
@@ -8,11 +10,22 @@ export default async function Blog({ params }: { params: { slug: string } }) {
   const post = await getPostBySlug(slug)
 
   return (
-    <main className="w-full max-w-4xl flex flex-col gap-4 py-12">
+    <main className="w-full max-w-4xl flex flex-col gap-4 py-6 lg:px-0 px-4">
       <Link href="/" className="flex gap-1 items-center">
         <FaLongArrowAltLeft /> Back to Home
       </Link>
       <h1 className="text-6xl">{post.title}</h1>
+      {post.preview?.asset && (
+        <div className="h-72 relative">
+          <Image
+            src={generateImageUrl(post.preview.asset._ref).url()}
+            alt="image"
+            fill
+            objectFit="cover"
+            className="bg-slate-100"
+          />
+        </div>
+      )}
       <PortableText
         value={post.body}
         components={{
